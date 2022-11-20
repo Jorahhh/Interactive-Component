22e9d2089221daeba98d866abf167758af696e3d
Angelo Barbarulo <angelobarbarulo8@gmail.com>
2022-11-20 18:53:07 +0100

    edit last little things

--- script/script.js
+++ script/script.js
@@ -7,17 +7,16 @@ const answer = document.getElementById('answer');
 
 const EventRatingsBtn = (e) => {
     e.target.classList.add('active');
-    let myValue =  e.target.textContent;
+    let myValue = e.target.textContent;
     if(e.target.classList.contains('active')){ 
         answer.textContent = myValue;
-        submBtn.addEventListener('click', () => { //when the event contains 'active' class, you can use submit button.
+        submBtn.addEventListener('click', () => {      //when the event contains 'active' class, you can use submit button.
             container.style.display = 'none';
             hiddenContainer.style.display = 'block';
             console.log('it works!')
         });
     }
 }
-
 
 ratings.forEach(btn => {
     btn.addEventListener('click', EventRatingsBtn);
