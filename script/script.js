35a3178e20f508c1721cdbc6a846297d7b1a3224
Angelo Barbarulo <angelobarbarulo8@gmail.com>
2022-11-20 12:23:34 +0100

    added the script and then ended the project

---
+++ script/script.js
@@ -0,0 +1,26 @@
+const submBtn = document.getElementById('submit');
+const ratings = document.querySelectorAll('.circle-button');
+const container = document.querySelector('.container');
+const hiddenContainer = document.querySelector('.hidden-container');
+const answer = document.getElementById('answer');
+
+
+const EventRatingsBtn = (e) => {
+    e.target.classList.add('active');
+    let myValue =  e.target.textContent;
+    if(e.target.classList.contains('active')){ 
+        answer.textContent = myValue;
+        submBtn.addEventListener('click', () => { //when the event contains 'active' class, you can use submit button.
+            container.style.display = 'none';
+            hiddenContainer.style.display = 'block';
+            console.log('it works!')
+        });
+    }
+}
+
+
+ratings.forEach(btn => {
+    btn.addEventListener('click', EventRatingsBtn);
+});
+
+
